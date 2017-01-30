4decfe57dee5ce4dc5e27063c897f1de509b65b3
freecates <ramongil@ramongil.cat>
2017-01-30 16:54:05 +0100

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // findOneAndUpdate
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('588f57bd312be0c0e8e8d52b')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('588b3f76f9f48e22ccedbf84')
+    }, {
+        $set: {
+            name: 'Ramon'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close();
+});
